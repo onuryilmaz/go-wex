73cae1516ec4749d2f5192fe8ff8ed2758480f10
Onur Yilmaz <onuryilmaz89@gmail.com>
2016-07-05 17:41:25 +0300

    Example usage

--- btce.go
+++ btce.go
@@ -1,10 +1,33 @@
-// Package btce
-// TODO package level documentation
-// Include example usage
+// Package btce provides native Go client for interacting with BTC-E Public API v3 and Trading API.
+//
+// Example usage:
+//
+// 	package main
+//
+// 	import (
+//		"fmt"
+//		btc "github.com/onuryilmaz/go-btce"
+// 	)
+//
+// 	func main() {
+//
+//		api := btc.API{}
+//
+//		ticker, err := api.Public.Ticker([]string{"btc_usd"})
+//		if err == nil {
+//			fmt.Printf("BTC buy price: %.3f \n", ticker["btc_usd"].Buy)
+//			fmt.Printf("BTC sell price: %.3f \n", ticker["btc_usd"].Sell)
+//		}
+//
+//		info, err := api.Trade.GetInfoAuth("API_KEY", "API_SECRET")
+//		if err == nil {
+//			fmt.Printf("BTC amount: %.3f \n", info.Funds["btc_usd"])
+//		}
+// 	}
 package btce
 
 // API allows to use public and trade APIs of BTC-E
 type API struct {
-	public PublicAPI
-	trade  TradeAPI
+	Public PublicAPI
+	Trade  TradeAPI
 }
